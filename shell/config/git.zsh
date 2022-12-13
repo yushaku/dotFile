92e5f3b667b16b8567b43982bafa3bc0aa2a6ff8
yushaku <son.lv@zinza.com.vn>
2022-12-13 23:24:32 +0700

    update git alias

--- shell/config/git.zsh
+++ shell/config/git.zsh
@@ -12,6 +12,8 @@ alias gpo="git push --set-upstream origin"
 
 alias gcl='git clone --recurse-submodules'
 
+alias gcb='git checkout -b'
+
 alias grb='git rebase'
 alias grba='git rebase --abort'
 alias grbc='git rebase --continue'
@@ -23,7 +25,23 @@ alias gcpc='git cherry-pick --continue'
 
 alias groh='git reset origin/$(git_current_branch) --hard'
 
+alias gst='git stash'
+alias gsta='git stash -u'
+alias gstp='git stash pop'
+
+alias gu="git pull -a"
+alias gur="git pull --rebase -v"
+
+alias gf="git fetch"
+alias gfa="git fetch --all"
+
+
+
 ##>> git custome function ---------------
+function git_current_branch () {
+  currentBranch=$(git rev-parse --abbrev-ref HEAD)
+  echo $currentBranch
+}
 
 function ggp() {
   if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
@@ -45,12 +63,12 @@ compdef _git _git_log_prettily=git-log
 
 function lazygit() {
   if ( $# -eq 0 ); then
-    git add --all
+    git add . 
     git commit --ament --no-edit
     git push -f
   else 
     git add --all
     git commit -m "$1"
-    git push
+    git push $(git_current_branch)
   fi
 }
