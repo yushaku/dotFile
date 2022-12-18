1632db3047f835083f2f8d4c257db34dd1146892
yushaku <son.lv@zinza.com.vn>
2022-12-18 23:27:57 +0700

    config

--- shell/config/fzf.zsh
+++ shell/config/fzf.zsh
@@ -5,3 +5,13 @@ export FZF_DEFAULT_COMMAND="fdfind --type file --color=always"
 export FZF_DEFAULT_OPTS="--ansi"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+_preview_search(){
+  fzf --preview='batcat --color="always" --style="numbers" {}' --bind shift-up:preview-page-up shift-down:preview-page-down
+}
+
+_preview_tree () {
+  fzf --preview='exa --tree -s -I='node_modules|dist|deploy' -L'
+}
+
+alias ff="_preview_search"
