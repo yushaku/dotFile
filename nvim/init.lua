61836452dc7d39a8963ac7cbc2bd5d62c10b6a28
yushaku <levanson180200@gmail.com>
2023-03-24 16:16:45 +0700

    remove nvim config

--- nvim/init.lua
+++
@@ -1,9 +0,0 @@
-require("plugins-setup")
-require("core.options")
-require("core.theme")
-require("core.keymaps")
-
-require("lsp.mason")
-require("lsp.lspsaga")
-require("lsp.lspconfig")
-require("lsp.null-ls")
--- nvim/lua/core/theme.lua
+++
@@ -1,5 +0,0 @@
-local status, _ = pcall(vim.cmd, "colorscheme onedark")
-if not status then
-	print("Colorscheme not found!") -- print error if colorscheme not installed
-	return
-end
