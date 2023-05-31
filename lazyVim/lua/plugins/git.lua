7567303245961a20b6e3c2e17508c4cfecb800c5
yushaku <levanson180200@gmail.com>
2023-05-31 16:16:54 +0700

    add diff view

--- lazyVim/lua/plugins/git.lua
+++ lazyVim/lua/plugins/git.lua
@@ -23,17 +23,33 @@ return {
       -- stylua: ignore start
       map("n", "]h", gs.next_hunk, "Next Hunk")
       map("n", "[h", gs.prev_hunk, "Prev Hunk")
-      map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-      map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-      map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+      map("n", "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+      map("n", "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
       map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-      map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+
+      map("n", "<leader>gha", gs.stage_buffer, "add file to Stage Buffer")
+      map("n", "<leader>ghR", gs.reset_buffer, "Reset current file on Buffer")
+
       map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
       map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
-      map("n", "<leader>ghd", gs.diffthis, "Diff This")
-      map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
       end,
+    },
+  },
+  {
+    "sindrets/diffview.nvim",
+    opts = {
+      signs = {
+        fold_closed = "",
+        fold_open = "",
+        done = "✓",
+      },
+    },
+    keys = {
+      { "<leader>gg", "<CMD>:DiffviewOpen<CR>", desc = "open git diff" },
+      { "<leader>gc", "<CMD>:DiffviewClose<CR>", desc = "close git diff" },
+      { "<leader>ge", "<CMD>:DiffviewToggleFiles<CR>", desc = "open changed files" },
+      { "<leader>gt", "<CMD>:DiffviewFileHistory<CR>", desc = "open git diff history" },
     },
   },
 }
