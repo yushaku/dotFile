f2eab0444a6321a0095197bf7603e23a1f85237b
yushaku <levanson180200@gmail.com>
2023-06-13 17:56:22 +0700

    add file browser for telescopr

--- lazyVim/lua/plugins/ai.lua
+++ lazyVim/lua/plugins/ai.lua
@@ -5,8 +5,8 @@ return {
     -- stylua: ignore
     config = function ()
       vim.g.codeium_idle_delay = 75
-      -- vim.g.codeium_disable_bindings = 1
-      vim.keymap.set("i", "<A-cr>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+      vim.g.codeium_disable_bindings = 1
+      vim.keymap.set("i", "<A-CR>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
       vim.keymap.set("i", "<A-f>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
       vim.keymap.set("i", "<A-b>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
       vim.keymap.set("i", "<A-e>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
---
+++ lazyVim/lua/plugins/telescope.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    version = false, -- telescope did only one release, so use HEAD for now
+    opts = {
+      extension = {
+        file_browser = {
+          theme = "ivy",
+          auto_depth = true,
+          depth = 2,
+          hidden = { file_browser = true, folder_browser = false },
+        },
+      },
+    },
+    keys = {
+      -- remove deault config of telescope
+      { "<leader><space>", false },
+      { "<leader>sh", false },
+      { "<leader>sH", false },
+      { "<leader>sk", false },
+      { "<leader>sT", false },
+      { "<leader>so", false },
+      { "<leader>sM", false },
+      { "<leader>sa", false },
+      { "<leader>ss", false },
+      { "<leader>sS", false },
+      { "<leader>sC", false },
+      { "<leader>sR", false },
+      { "<leader>sd", false },
+      { "<leader>sD", false },
+      { "<leader>sg", false },
+      { "<leader>sG", false },
+      { "<leader>fe", false },
+      { "<leader>fE", false },
+      { "<leader>ft", false },
+      { "<leader>fT", false },
+      { "<leader>fn", false },
+
+      -- create my own config
+      {
+        "<C-p>",
+        "<cmd>lua require 'telescope'.extensions.file_browser.file_browser()<CR>",
+        desc = "Find Files (root dir)",
+      },
+      { "<leader>ft", "<CMD>TodoTelescope<cr>", desc = "Open todo list by telescope" },
+      { "<leader>fr", "<CMD>Telescope resume<cr>", desc = "resume privious search result" },
+    },
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  },
+}
